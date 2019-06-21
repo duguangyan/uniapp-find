f8dfef11cf954b334ec06cdead7968df66b27ce9
Riicy <riicy@RiicydeMac-mini.local>
2019-06-21 12:36:46 +0800

    设置

--- pages/setting/setting.vue
+++ pages/setting/setting.vue
@@ -1,6 +1,25 @@
 <template>
-	<view>
-		设置
+	<view class="index">
+		<view class="setting_list">
+			<view class="item_first">
+				1
+			</view>
+			<view class="item">
+				2
+			</view>
+			<view class="item">
+				3
+			</view>
+			<view class="item">
+				4
+			</view>
+			<view class="item">
+				5
+			</view>
+			<view class="item">
+				6
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -17,6 +36,24 @@
 	}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+	.index{
+		background-color: #F5F5F5;
+	}
+	.setting_list{
+			background-color: orange;
+			display: flex;
+			display: -webkit-flex;
+			flex-direction: column;
+			height: 800upx;
+			align-content: space-between;
+			.item{
+				background-color: green;
+				flex-basis: 116upx;
+			}
+			.item_first{
+				background-color: red;
+				flex-basis: 180upx;
+			}
+		}
 </style>
