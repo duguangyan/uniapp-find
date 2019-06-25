18258830542a5d5839f223907a267644f26b0402
duguangyan <dis2010@126.com>
2019-06-25 17:03:24 +0800

    x

--- pages/index/pages/order.vue
+++ pages/index/pages/order.vue
@@ -188,12 +188,7 @@
 			 focus="{{isFocus}}" bindinput="Focus"></input>
 			<view class='text-theme fs30 flr pdr-30 mgb-40' catchtap='forgetPayPassWord'>忘记密码</view>
 		</form> -->
-
-<<<<<<< HEAD
 	</view>
-=======
-
->>>>>>> 84f85124cc71a052d689b8e5b0687d5b4fd1a868
 </template>
 
 <script>
